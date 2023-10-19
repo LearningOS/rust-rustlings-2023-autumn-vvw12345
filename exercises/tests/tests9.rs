31a07e880dec694d0c8e5dd540296f4fc429b0eb
yzw12138~ <112001227+vvw12345@users.noreply.github.com>
2023-10-19 14:10:48 +0000

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,11 +27,12 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
+
 
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
@@ -39,8 +40,8 @@ extern "Rust" {
 mod foo {
     // No `extern` equals `extern "Rust"`.
     #[no_mangle]
-    #[link_name = "my_demo_function_alias"]
-    pub fn my_demo_function(a: u32) -> u32 {
+    //#[link_name = "my_demo_function_alias"]
+    pub extern "Rust" fn my_demo_function(a: u32) -> u32 {
         a
     }
 }
